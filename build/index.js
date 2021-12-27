099bfb678478d388d0cd56dc4e6da62694a6e91a
yomarguti <yomar.guti@gmail.com>
2021-12-27 09:04:39 -0500

    Initial commit

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log('Hi there!');
+console.log('New call to tsc');
+const logSomething = () => {
+    console.log('New console log function');
+};
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log('Hi there!');
+console.log('New call to tsc');
+
+const logSomething = () => {
+  console.log('New console log function');
+};
